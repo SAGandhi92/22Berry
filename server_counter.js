f141460b5eac49eb955a94701eb482ae6ce39ede
Salonee Gandhi <gandhin92@gmail.com>
2015-06-22 15:31:06 -0400

    first commit

---
+++ server_counter.js
@@ -0,0 +1,32 @@
+var express        = require('express')
+    server_counter = require (),
+    bodyParser     = require ('body-parser'),
+    ejs            = require('ejs')
+    MongoClient    = require('mongodb').MongoClient,
+    ObjectId       = require('mongodb').ObjectId,
+    url            = 'mongodb://localhost:27017/'
+    methodOverride = require ('method-override')
+    ;
+
+server_counter.set('views', "./views");
+server_counter.set('view engine', "ejs");
+
+server_counter.use();
+server_counter.use(bodyParser.urlencoded{extended: true}));
+server.use(methodOverride("_method"))
+
+
+//INDEX
+server.get();
+
+//CREATE
+server.post();
+
+//SHOW
+server.new();
+
+//UPDATE
+server.patch();
+
+//DELETE
+server.delete();
