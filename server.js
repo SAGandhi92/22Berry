bfb6fafec288311b22b116358ad4e81882206744
Derek Jacobi <derekjacobi@gmail.com>
2015-06-23 16:12:12 -0400

    Server.js fixed

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 var express        = require('express')
-    server         = require (),
-    bodyParser     = require ('body-parser'),
-    ejs            = require('ejs')
+    server         = express(),
+    bodyParser     = require('body-parser'),
+    ejs            = require('ejs'),
     MongoClient    = require('mongodb').MongoClient,
     ObjectId       = require('mongodb').ObjectId,
-    url            = 'mongodb://localhost:27017/'
+    url            = 'mongodb://localhost:27017/',
     methodOverride = require ('method-override');
 
 server.set('views', "./views");
 server.set('view engine', "ejs");
 
-server.use();
 server.use(bodyParser.urlencoded({extended: true}));
-server.use(methodOverride("_method"))
+server.use(methodOverride("_method"));
 
-
-//INDEX
-server.get();
-
-//CREATE
-server.post();
-
-//SHOW
-server.new();
-
-//UPDATE
-server.patch();
-
-//DELETE
-server.delete();
+server.listen(3000, function(){
+  console.log("Server Ready");
+})
+//
+// //INDEX
+// server.get();
+//
+// //CREATE
+// server.post();
+//
+// //SHOW
+// server.new();
+//
+// //UPDATE
+// server.patch();
+//
+// //DELETE
+// server.delete();
